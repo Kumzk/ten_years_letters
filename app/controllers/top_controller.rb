8ef8bf7de020f0fea61c6350ccf855b5c1a717bc
熊崎章吾 <white.march322@gmail.com>
2018-11-03 11:10:57 +0900

    deviseアカウント作成へのパス

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,10 +1,7 @@
 class TopController < ApplicationController
-  before_action :move_to_index, except: :index
+
   def index
   end
 
-  private
-  def move_to_index
-   redirect_to :action => "index" unless user_signed_in?
- end
+
 end
