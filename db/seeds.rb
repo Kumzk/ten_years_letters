52a4062ae41b83a8bfd7e3257ca79559f640e593
熊崎章吾 <white.march322@gmail.com>
2018-11-03 12:34:30 +0900

    seeds.rbの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# userstable seeds
+ User.create(id: 1, email: 'test@gmail.com',encrypted_password: '1111111',name: 'test', birthday: 2000-01-01, status: 0 )
+ #typetableseeds
+ #Type.create(id: 1, name: '誕生日', url: '')
+ #Type.create(id: 2, name: '記念日', url: '')
+ #Type.create(id: 3, name: '思い出の日', url: '')
