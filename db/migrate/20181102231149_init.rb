36240184cf1907ff136802a31aa22d4901dacb44
熊崎章吾 <white.march322@gmail.com>
2018-11-04 10:54:21 +0900

    nameカラム追加

--- db/migrate/20181102231149_init.rb
+++ db/migrate/20181102231149_init.rb
@@ -1,12 +1,13 @@
 class Init < ActiveRecord::Migration[5.1]
   create_table :messages do |t|
+    t.string :name, null: false
     t.text :email, null: false
     t.string :image, null: false
     t.date :when_message, null: false
     t.integer :status, null: false, default: 1
     t.text :answer1, null: false
     t.text :answer2, null: false
-    t.string :name, null: false
+
 
     t.integer :question_id, null: false
     t.integer :user_id, null: false
